f21ca7e31d18d772086d6544e55716a32c16c132
George <qiaozhi_china@163.com>
2016-07-06 21:33:26 +0800

    加了第三方模块，添加stepview materialtabs

--- app/src/main/java/com/is/see/base/Retrofit2See.java
+++ app/src/main/java/com/is/see/base/Retrofit2See.java
@@ -2,6 +2,13 @@ package com.is.see.base;
 
 import com.is.see.api.ApiConstants;
 
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -15,10 +22,24 @@ public class Retrofit2See {
     protected ApiConstants apiConstants;
 
     public Retrofit2See() {
-        retrofit = new Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+        if (retrofit==null) {
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+            /**
+             * 设置公共参数
+             */
+            builder.addInterceptor(addQueryParameterInterceptor());
+            /**
+             * 设置头
+             */
+            builder.addInterceptor(addHeaderInterceptor());
+
+            OkHttpClient client = builder.build();
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build();
+        }
         apiConstants = retrofit.create(ApiConstants.class);
     }
 
@@ -29,4 +50,47 @@ public class Retrofit2See {
     public void setBaseUrl(String baseUrl) {
         this.baseUrl = baseUrl;
     }*/
+
+    /**
+     * 设置公共参数
+     */
+    private static Interceptor addQueryParameterInterceptor() {
+        Interceptor addQueryParameterInterceptor = new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request originalRequest = chain.request();
+                Request request;
+                HttpUrl modifiedUrl = originalRequest.url().newBuilder()
+                        // Provide your custom parameter here
+                        .addQueryParameter("platform", "android")
+                        .addQueryParameter("version", "1.0.0")
+                        .build();
+                request = originalRequest.newBuilder().url(modifiedUrl).build();
+                return chain.proceed(request);
+            }
+        };
+        return addQueryParameterInterceptor;
+    }
+
+    /**
+     * 设置头
+     */
+    private static Interceptor addHeaderInterceptor() {
+        Interceptor headerInterceptor = new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request originalRequest = chain.request();
+                Request.Builder requestBuilder = originalRequest.newBuilder()
+                        // Provide your custom header here
+                        .header("AppType", "TPOS")
+                        .header("Accept", "application/json")
+                        .method(originalRequest.method(), originalRequest.body());
+                Request request = requestBuilder.build();
+                return chain.proceed(request);
+            }
+        };
+        return headerInterceptor;
+    }
+
+
 }
